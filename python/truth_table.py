e11062f65616c2156b01e3f1229572758e582a0a
SPOOK_EXE <91944990+SPOOKEXE@users.noreply.github.com>
2024-02-23 19:33:03 +0930

    regex-adds

--- python/truth_table.py
+++ python/truth_table.py
@@ -1,4 +1,9 @@
 
+# use chat-gpt for REGEX PATTERNS
+# https://chat.openai.com/c/41e7b086-9f66-4af0-9029-49d6e8d8b660
+
+# also add a better method of testing (such as calculating the actual outputs then assertions/warns)
+
 import numpy as np
 import json
 import re
@@ -13,7 +18,7 @@ def bin_to_bool_expression( binary_in : str ) -> str:
 
 def truth_table_single_output( truth_table_rows : list[str], output : str ) -> list[str]:
 	'''
-	A | B | C | D | Z1
+	A | B | C | D | Z
 	------------------
 	0 | 0 | 0 | 0 | 1
 	0 | 0 | 0 | 1 | 0
@@ -78,37 +83,37 @@ SIMPLIFY_REGEX_TUPLES : tuple[ str, Callable[[tuple], str], Callable[[tuple], st
 		# NOT(NOT(*)) -> (*)
 		r'NOT\(NOT\((.*?)\)\)',
 		lambda values : ''.join(values),
-		lambda value : re.match(r'(?<=NOT\()NOT\(([A-Z])\)(?=\))', value) or re.match(r'NOT\(NOT\((.)\) AND NOT\((.)\)\)', value) != None
+		lambda value : re.match(r'(?<=NOT\()NOT\(([A-Z])\)(?=\))', value) or re.match(r'NOT\((?:A AND NOT\(B\)|NOT\(A\) AND B)\)', value) != None
+	),
+	(
+		# NOT(NOT(A) AND NOT(B) AND NOT(C) AND NOT(D) AND NOT(E)) -> (A OR B OR C OR D OR E)
+		r'NOT\(NOT\(([^)]+)\)\)',
+		lambda values : print(values),# ' OR '.join(values),
+		lambda value : re.match(r'NOT\(NOT\(([^)]+)\)\)', value) or re.match(r'NOT\((?:A AND NOT\(B\)|NOT\(A\) AND B)\)', value) != None
 	),
 	(
 		# NOT(A) AND NOT(B) AND NOT(C) -> NOT(A OR B OR C)
 		r'(?<=NOT\()[A-Z](?=\))',
 		lambda values : 'NOT(' + (' OR '.join(values)) + ')',
-		lambda value : re.fullmatch(r'(?<=NOT\()[A-Z](?=\))', value) or re.match(r'NOT\(NOT\((.)\) AND NOT\((.)\)\)', value) != None
+		lambda value : re.fullmatch(r'(?<=NOT\()[A-Z](?=\))', value) or re.match(r'NOT\((?:A AND NOT\(B\)|NOT\(A\) AND B)\)', value) != None
 	),
 	(
 		# NOT(NOT(A) AND NOT(B) AND NOT(C) AND NOT(D)) -> NOT( NOT(A OR B) ) -> (A OR B) # IGNORE NOT(NOT(*)) PART
 		r'(?<=NOT\()[A-Z](?=\))',
 		lambda values : ' OR '.join(values), # skip the NOT( NOT( * ) ) step
-		lambda value : re.match(r'NOT\(NOT\((.)\) AND NOT\((.)\)\)', value)
+		lambda value : re.match(r'NOT\(NOT\((.)\) AND NOT\((.)\)\)', value) or re.match(r'NOT\((?:A AND NOT\(B\)|NOT\(A\) AND B)\)', value) != None
 	),
 	(
 		# NOT(A AND NOT(B)) -> NOT(A) OR B
-		r'NOT\((.) AND NOT\((.)\)\)',
+		r'NOT\((\w+) AND NOT\((\w+)\)\)',
 		lambda values : f'NOT({values[0]}) OR {values[1]}',
-		lambda value : re.match( r'NOT\([A-Za-z]\sAND\sNOT\([A-Za-z]\)\)', value )
+		lambda value : re.match( r'NOT\((\w+) AND NOT\((\w+)\)\)', value )
 	),
 	(
 		# NOT(NOT(A) AND B) -> A OR NOT(B)
-		r'NOT\(NOT\((.)\) AND (.)\)',
+		r'NOT\(NOT\((\w+)\) AND (\w+)\)',
 		lambda values : f'{values[0]} OR NOT({values[1]})',
-		lambda value : re.match( r'NOT\(NOT\([A-Za-z]\)\sAND\s[A-Za-z]\)', value )
-	),
-	(
-		# NOT(NOT(A) AND NOT(B) AND NOT(C) AND NOT(D) AND NOT(E)) -> (A OR B OR C OR D OR E)
-		r'(?<=NOT\(NOT\()[A-E](?=\))',
-		lambda values : ' OR '.join(values),
-		lambda value : re.match(r'NOT\(NOT\([A-Z]\)(?:\sAND\sNOT\([A-Z]\))+\)', value)
+		lambda value : re.match( r'NOT\(NOT\((\w+)\) AND (\w+)\)', value )
 	),
 ]
 
@@ -132,9 +137,12 @@ def simplify_boolean_operation( boolean_op : str ) -> str:
 				value : list[str] = [ value.group(0) for value in matches ]
 			else:
 				value : list[str] = matches[0].groups()
+				if len(value) == 0:
+					return matches[0].string
+			# print(value)
 			hasSimplified = True
 			output : str = transform( value )
-			print(value, '->', output)
+			# print(value, '->', output)
 			start : int = matches[0].pos
 			finish : int = matches[-1].endpos
 			boolean_op = boolean_op[:start] + output + boolean_op[finish:]
@@ -157,18 +165,23 @@ def calculate_operations( value : str ) -> int:
 	return total
 
 if __name__ == '__main__':
-	value1 = 'NOT(A) AND NOT(B) AND NOT(C) AND NOT(D)'
-	print( value1, '>>', simplify_boolean_operation( value1 )  )
-	value2 = 'NOT(A) AND NOT(B)'
-	print( value2, '>>', simplify_boolean_operation( value2 )  )
-	value3 = 'NOT(NOT(A OR B))'
-	print( value3, '>>', simplify_boolean_operation( value3 )  )
-	value4 = 'NOT(NOT(A) AND NOT(B))'
-	print( value4, '>>', simplify_boolean_operation( value4 )  )
-	value5 = 'NOT(A AND NOT(B))'
-	print( value5, '>>', simplify_boolean_operation( value5 )  )
-	value6 : str = 'NOT(NOT(A) AND NOT(B) AND NOT(C))'
-	print( value6, '>>', simplify_boolean_operation( value6 )  )
+
+	TEST_BRANCHES : dict[str, str] = {
+		'NOT(A) AND NOT(B) AND NOT(C) AND NOT(D)' : 'NOT(A OR B OR C OR D)',
+		'NOT(A) AND NOT(B)' : 'NOT(A OR B)',
+		'NOT(NOT(A OR B))' : 'A OR B',
+		'NOT(NOT(A) AND NOT(B))' : 'A OR B',
+		'NOT(A AND NOT(B))' : 'NOT(A) OR B',
+		'NOT(NOT(A) AND NOT(B) AND NOT(C))' : 'A OR B OR C',
+		'NOT(NOT(A) AND B)' : 'A OR NOT(B)'
+	}
+
+	for inp, exp in TEST_BRANCHES.items():
+		out : str = simplify_boolean_operation( inp )
+		if exp==out:
+			print('Condition Passed: ', inp, out)
+		else:
+			print('Condition Failed:', inp, 'GOT', out, 'EXPECTING', exp )
 
 	# test = truth_table_multi_output(['00', '01', '10', '11'], ['0100', '1001', '0010'])
 	# print( 'BEFORE SIMPLIFY: ', json.dumps(test, indent=4) )
